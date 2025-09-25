59f0f8a8c0802e7e7374ab15d6889f3710c6a4ea
Robyn <rnixon98@gmail.com>
2025-09-24 20:18:45 -0500

    feat: add gender icon to humanoid entities

--- src/Soulash2Explorer/EntityListItem.cs
+++ src/Soulash2Explorer/EntityListItem.cs
@@ -8,6 +8,7 @@
 */
 
 using System;
+using System.IO;
 using Godot;
 using SoulashSaveUtils.Types;
 
@@ -27,13 +28,36 @@ public partial class EntityListItem : PanelContainer
   [Export]
   public PackedScene PortraitLayer;
 
+  [Export]
+  public Sprite2D GenderIcon;
+
   public event Action<int> EntityHistoryRequested;
   protected int EntityID;
+  protected static ImageTexture GenderTexture = new();
+  protected static bool LoadedTex = false;
 
   public override void _Ready()
   {
     Visible = false;
     ViewButton.Pressed += OnPressedView;
+
+    if (!LoadedTex)
+    {
+      string texPath = Path.Combine(SoulashSaveUtils.Helpers.Paths.DataPath, "core_2", "assets", "gfx", "portraits", "sex.png");
+      Image image = new();
+
+      LoggingWindow.Instance.LogMessage($"Loading {texPath}");
+      if (image.Load(texPath) != Error.Ok)
+      {
+        LoggingWindow.Instance.LogError("Unable to load gender icon file");
+        return;
+      }
+
+      GenderTexture.SetImage(image);
+      LoadedTex = true;
+    }
+
+    GenderIcon.Texture = GenderTexture;
   }
 
   public void Clear()
@@ -44,6 +68,7 @@ public partial class EntityListItem : PanelContainer
     Desc.Text = string.Empty;
     EntityID = -1;
 
+    GenderIcon.Visible = false;
     Visible = false;
   }
 
@@ -55,6 +80,21 @@ public partial class EntityListItem : PanelContainer
 
     Desc.Text = ent.GetFullName;
     EntityID = ent.EntityID;
+
+    if (ent.Components.TryGetValue("Humanoid", out var comp) && comp is HumanoidComponent humanoid)
+    {
+      switch (humanoid.Gender)
+      {
+        case HumanoidGender.Male:
+          GenderIcon.RegionRect = new(0, 0, 15, 15);
+          GenderIcon.Visible = true;
+          break;
+        case HumanoidGender.Female:
+          GenderIcon.RegionRect = new(15, 0, 15, 15);
+          GenderIcon.Visible = true;
+          break;
+      }
+    }
 
     Visible = true;
   }
